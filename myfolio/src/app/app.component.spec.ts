072d638e44261e566c0364a79deb97220d4cdc94
anup <anupkuppasagoudar@gmail.com>
2026-05-24 13:05:10 +0530

    updated v 18

--- myfolio/src/app/app.component.spec.ts
+++ myfolio/src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
     declarations: [AppComponent]
   }));
 
@@ -12,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myfolio'`, () => {
+  it(`should have as title 'MyFolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myfolio');
+    expect(app.title).toEqual('MyFolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('myfolio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('MyFolio app is running!');
   });
 });
